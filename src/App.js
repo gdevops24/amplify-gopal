fd76e33123754920443fea24d687b426e8150d9e
gopal <gopalm@zimetrics.com>
2024-10-14 20:26:14 +0530

    Final Push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {
+  PetProfileCollection 
+ } from './ui-components';
+
+ import {
+  NavBarHeader 
+ } from './ui-components';
+
+ import { withAuthenticator } from '@aws-amplify/ui-react';
 
 function App() {
+const NavBarHeader1 = {
+Image :{
+  src : "https://encrypted-tbn0.gstatic.com/images?q=tbn:AN…WHQkSe-poxgVYcOEqz4Vmg9awAHZrsEbUNg0ZHQw&usqp=CAU" 
+}
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <NavBarHeader overrides = {NavBarHeader1} />
+
+
+      <PetProfileCollection />
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
--- src/index.js
+++ src/index.js
@@ -4,11 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+/* later added */
+//import { ThemeProvider } from "@aws-amplify/ui-react";
+import { ThemeProvider, defaultTheme } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure(awsconfig);
+
+
+/* later added */
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+  <ThemeProvider theme={defaultTheme}>
+  <App />
+</ThemeProvider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
